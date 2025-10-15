8fce94a40c056514f4500167f95696d79685932c
kritikavedecc1984-droid <kritikavedecc1984@gmail.com>
2025-10-15 23:21:04 +0300

    Add files via upload

---
+++ TrafficLight/AdaptiveTrafficLight.java
@@ -0,0 +1,20 @@
+package TrafficLight;
+
+// Адаптивный светафор
+/*public class AdaptiveTrafficLight implements SuperLight2 {
+    private int carCount; // кол-во авто
+    private int maxCarThreshold; // максимальный порог автомобиля
+    private boolean sensorActive; // активность светофора
+
+
+    @Override
+    public void light() {
+        System.out.println("дал свет");
+
+    }
+
+    //solid interface segregation
+    //s-
+    //o - open|closed
+    //i - interface
+}*/
